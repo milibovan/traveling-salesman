73b9a45e36abe4d9ebcace98fc032e512fec36d4
milibovan <milibovan190d@gmail.com>
2025-08-10 22:58:52 +0200

    Added pseudocode for genetic algorithm and added reading from file and created struct Route. TODO Implement GA

--- traveling-salesman/src/main.rs
+++ traveling-salesman/src/main.rs
@@ -1,3 +1,32 @@
+use std::collections::HashSet;
+use std::fs;
+
+mod genetic_algorithm;
+
+#[derive(Debug)]
+struct Route {
+    source: String,
+    destination: String,
+    distance: i32
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let input = fs::read_to_string("input.txt").expect("Something went wrong while reading the file");
+    let split = input.split("\n").collect::<Vec<&str>>();
+    let mut cities = HashSet::new();
+    let mut routes = <Vec<Route>>::new();
+
+    for relation in split {
+        let row = relation.split(",").collect::<Vec<&str>>();
+        cities.insert(row[0].to_string());
+        routes.push(Route {
+            source: row[0].parse().unwrap(),
+            destination: row[2].parse().unwrap(),
+            distance: row[4].parse().unwrap()
+        });
+    }
+
+    println!("{:?}", cities);
+    println!("{:?}", routes);
 }
