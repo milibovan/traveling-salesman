8cd8fc2c7a3b94990db1a623a306eb0fc17dcad3
milibovan <milibovan190d@gmail.com>
2025-08-11 19:51:14 +0200

    Refactored code and add code for selecting starting city

--- traveling-salesman/src/main.rs
+++ traveling-salesman/src/main.rs
@@ -1,9 +1,10 @@
 use std::collections::HashSet;
 use std::fs;
+use rand::{rng, Rng};
 
 mod genetic_algorithm;
 
-#[derive(Debug)]
+#[derive(Debug, Eq, Hash, PartialEq)]
 struct Route {
     source: String,
     destination: String,
@@ -11,22 +12,35 @@ struct Route {
 }
 
 fn main() {
+    let (cities, routes) = read_cities_and_routes();
 
+    let starting_city = get_starting_city(cities);
+    println!("Starting city: {:?}", starting_city);
+    // println!("{:?}", cities);
+    // println!("{:?}", routes);
+}
+
+fn get_starting_city(cities: HashSet<String>) -> String {
+    let cities_vec: Vec<_> = cities.iter().collect();
+    let index = rng().random_range(0..cities_vec.len());
+
+    cities_vec[index].clone()
+}
+
+fn read_cities_and_routes() -> (HashSet<String>, HashSet<Route>) {
     let input = fs::read_to_string("input.txt").expect("Something went wrong while reading the file");
     let split = input.split("\n").collect::<Vec<&str>>();
     let mut cities = HashSet::new();
-    let mut routes = <Vec<Route>>::new();
+    let mut routes = <HashSet<Route>>::new();
 
     for relation in split {
         let row = relation.split(",").collect::<Vec<&str>>();
         cities.insert(row[0].to_string());
-        routes.push(Route {
+        routes.insert(Route {
             source: row[0].parse().unwrap(),
             destination: row[2].parse().unwrap(),
             distance: row[4].parse().unwrap()
         });
     }
-
-    println!("{:?}", cities);
-    println!("{:?}", routes);
+    (cities, routes)
 }
